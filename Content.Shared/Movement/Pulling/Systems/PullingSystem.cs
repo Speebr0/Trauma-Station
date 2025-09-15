b9846a478ef789e4bf766fc37a29f5c3603579ef
Errant <35878406+Errant-4@users.noreply.github.com>
2025-09-15 13:31:22 +0200

    [Hotfix] Remove pull-escape trick (#40368)
    * make HandleStopPull byref
    
    * we get signal

--- Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
+++ Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
@@ -931,7 +931,7 @@ public sealed class PullingSystem : EntitySystem
             return true;
 
         var msg = new AttemptStopPullingEvent(user);
-        RaiseLocalEvent(pullableUid, ref msg, true); // Goob edit
+        RaiseLocalEvent(pullableUid, ref msg, true);
 
         if (msg.Cancelled)
             return false;
