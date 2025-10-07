924b5eb5744455ed87436d95b5e16c54df569ab1
Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
2025-10-07 09:10:21 -0400

    Move BrainSystem and necessary components to Shared (#40499)
    * yowza
    
    * Update Content.Shared/Body/Systems/BrainSystem.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Update Content.Shared/Ghost/GhostOnMoveComponent.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Update BrainSystem.cs
    
    * Update Content.Shared/Body/Systems/BrainSystem.cs
    
    * Update Content.Shared/Body/Systems/BrainSystem.cs
    
    * Update Content.Shared/Body/Systems/BrainSystem.cs
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/Body/Systems/BrainSystem.cs
+++ Content.Shared/Body/Systems/BrainSystem.cs
@@ -1,28 +1,6 @@
-// SPDX-FileCopyrightText: 2021 20kdc <asdd2808@gmail.com>
-// SPDX-FileCopyrightText: 2021 Vera Aguilera Puerto <gradientvera@outlook.com>
-// SPDX-FileCopyrightText: 2022 Jezithyr <Jezithyr@gmail.com>
-// SPDX-FileCopyrightText: 2022 mirrorcult <lunarautomaton6@gmail.com>
-// SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 Jezithyr <jezithyr@gmail.com>
-// SPDX-FileCopyrightText: 2023 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 metalgearsloth <comedian_vs_clown@hotmail.com>
-// SPDX-FileCopyrightText: 2024 0x6273 <0x40@keemail.me>
-// SPDX-FileCopyrightText: 2024 Krunklehorn <42424291+Krunklehorn@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
-// SPDX-FileCopyrightText: 2024 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
-// SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-using Content.Server.Body.Components;
-using Content.Server.Ghost.Components;
+using Content.Shared.Body.Components;
 using Content.Shared.Body.Events;
+using Content.Shared.Ghost;
 using Content.Shared.Mind;
 using Content.Shared.Mind.Components;
 using Content.Shared.Mobs.Components;
@@ -32,7 +10,7 @@ using Content.Shared.Pointing;
 using Content.Shared.Body.Systems;
 using Content.Goobstation.Common.Changeling;
 
-namespace Content.Server.Body.Systems;
+namespace Content.Shared.Body.Systems;
 
 public sealed class BrainSystem : EntitySystem
 {
@@ -72,6 +50,7 @@ public sealed class BrainSystem : EntitySystem
             return;
 
         brain.Active = false;
+        Dirty(uid, brain);
         if (!HasBrain(args.OldBody))
         {
             // Prevents revival, should kill the user within a given timespan too.
@@ -86,6 +65,7 @@ public sealed class BrainSystem : EntitySystem
             return;
 
         brain.Active = true;
+        Dirty(uid, brain);
         if (HasBrain(args.Body))
         {
             RemComp<DebrainedComponent>(args.Body);
