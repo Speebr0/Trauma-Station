93a18a5f61a194f871ef85abc2f18af439e45443
eoineoineoin <helloworld@eoinrul.es>
2025-10-21 23:01:22 +0100

    Fix refresh button in fax machine (#41024)

--- Content.Client/Stylesheets/StyleClass.cs
+++ Content.Client/Stylesheets/StyleClass.cs
@@ -56,6 +56,7 @@ public static class StyleClass
     public const string ButtonBig = "ButtonBig";
 
     public const string CrossButtonRed = "CrossButtonRed";
+    public const string RefreshButton = "RefreshButton";
 
     public const string ItemStatus = "ItemStatus";
     public const string ItemStatusNotHeld = "ItemStatusNotHeld";
