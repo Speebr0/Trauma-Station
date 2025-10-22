2a9f669816c503ea014502272de93835526aaf7e
deltanedas <@deltanedas:kde.org>
2025-10-22 13:25:32 +0100

    remove conflict bait from entity effects

--- Content.Server/EntityEffects/Effects/PlantMetabolism/PlantRestoreSeeds.cs
+++ Content.Server/EntityEffects/Effects/PlantMetabolism/PlantRestoreSeeds.cs
@@ -1,9 +1,3 @@
-// SPDX-FileCopyrightText: 2024 Aidenkrz <aiden@djkraz.com>
-// SPDX-FileCopyrightText: 2024 Eris <eris@erisws.com>
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 using Content.Server.Botany.Components;
 using Content.Server.Botany.Systems;
 using Content.Shared.EntityEffects;
