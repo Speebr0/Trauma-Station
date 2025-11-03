madeline-celeste/Maidstation

The great surveillance cameras fixing (#4706)
* I probably should've tried to do this more incrementally but oh well.
Fixed client side switching effect system to use timing properly.
Made camera switching a bit faster.
Made camera heartbeat maximum durantion be 5 seconds, which means 5 seconds after connection is lost, camera is disconnected.
Fixed a bug where heartbeat force-disconnect would brick the camera monitor: not being able to connect to any cameras.
Added a new variable to camera component which determines if a camera is mobile or not. It is used to determine how camera is processed and stored as well as appearance on the monitor.
Made mobile cameras override PVS of camera monitor users that can see them. Tested as many edge cases as I could, seemed fine.
Finally, I also did some general refactoring stuff.

* Update ActiveSurveillanceCameraMonitor.cs

Forgot one goobstation comment, whoops.

* Goobstation comments fix and fixed a mistake in thirdQuery. Was supposed to remove HasMobileCamerasSurveillanceCameraMonitorComponent and not ReconnectingSurveillanceCameraMonitorComponent.

* Addressing requested changes.

* Addressing requested changes.

* Added camera name to the status label.

* Added a blinking blip for camera monitor location, like other monitors have.

Content.Server/SurveillanceCamera/Systems/SurveillanceCameraSystem.cs
@@ -70,6 +70,7 @@ public sealed class SurveillanceCameraSystem : EntitySystem
     [Dependency] private readonly DeviceNetworkSystem _deviceNetworkSystem = default!;
     [Dependency] private readonly UserInterfaceSystem _userInterface = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly TransformSystem _transformSystem = default!; // Goobstation
 
     // Pings a surveillance camera subnet. All cameras will always respond
     // with a data message if they are on the same subnet.
@@ -98,6 +99,7 @@ public sealed class SurveillanceCameraSystem : EntitySystem
     public const string CameraNameData = "surveillance_camera_data_name";
     public const string CameraSubnetData = "surveillance_camera_data_subnet";
     public const string CameraNetEntity = "surveillance_camera_net_entity"; // Goobstation
+    public const string CameraMobile = "surveillance_camera_mobile"; // Goobstation - Is the camera mobile? Needed for pvs sorting as well as the icon in the camera monitor
 
     public const int CameraNameLimit = 32;
 
@@ -163,15 +165,16 @@ public sealed class SurveillanceCameraSystem : EntitySystem
                     {
                         return;
                     }
-
                     dest = args.SenderAddress;
                     payload[CameraSubnetData] = subnet;
                     // Goobstation start
-                    TransformComponent? transformComponent;
-                    TryComp(uid, out transformComponent);
-                    if (transformComponent != null)
+                    if (TryComp(uid, out TransformComponent? transformComponent))
                     {
-                        payload[CameraNetEntity] = (GetNetEntity(uid), GetNetCoordinates(transformComponent.Coordinates));
+                        // Decoupling the bodycam/nopro from the wearer, otherwise we'll just keep seeing the last known owner move around on the map
+                        payload[CameraNetEntity] = component.Mobile ?
+                            (GetNetEntity(uid), GetNetCoordinates(_transformSystem.ToCoordinates(uid, _transformSystem.ToMapCoordinates(transformComponent.Coordinates)))) :
+                            (GetNetEntity(uid), GetNetCoordinates(transformComponent.Coordinates));
+                        payload[CameraMobile] = component.Mobile;
                     }
                     // Goobstation end
                     payload[DeviceNetworkConstants.Command] = CameraDataMessage;

Speebr0/Trauma-Station

Content.Server/SurveillanceCamera/Systems/SurveillanceCameraSystem.cs
@@ -16,13 +16,13 @@ namespace Content.Server.SurveillanceCamera;
 
 public sealed class SurveillanceCameraSystem : SharedSurveillanceCameraSystem
 {
+    [Dependency] private readonly SharedTransformSystem _transform = default!; // Goobstation
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly ViewSubscriberSystem _viewSubscriberSystem = default!;
     [Dependency] private readonly DeviceNetworkSystem _deviceNetworkSystem = default!;
     [Dependency] private readonly UserInterfaceSystem _userInterface = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
     [Dependency] private readonly IAdminLogManager _adminLogger = default!;
-
 
     // Pings a surveillance camera subnet. All cameras will always respond
     // with a data message if they are on the same subnet.
@@ -51,6 +51,7 @@ public sealed class SurveillanceCameraSystem : SharedSurveillanceCameraSystem
     public const string CameraNameData = "surveillance_camera_data_name";
     public const string CameraSubnetData = "surveillance_camera_data_subnet";
     public const string CameraNetEntity = "surveillance_camera_net_entity"; // Goobstation
+    public const string CameraMobile = "surveillance_camera_mobile"; // Goobstation - Is the camera mobile? Needed for pvs sorting as well as the icon in the camera monitor
 
     public const int CameraNameLimit = 32;
 
@@ -114,15 +115,16 @@ public sealed class SurveillanceCameraSystem : SharedSurveillanceCameraSystem
                     {
                         return;
                     }
-
                     dest = args.SenderAddress;
                     payload[CameraSubnetData] = subnet;
                     // Goobstation start
-                    TransformComponent? transformComponent;
-                    TryComp(uid, out transformComponent);
-                    if (transformComponent != null)
+                    if (TryComp(uid, out TransformComponent? transformComponent))
                     {
-                        payload[CameraNetEntity] = (GetNetEntity(uid), GetNetCoordinates(transformComponent.Coordinates));
+                        // Decoupling the bodycam/nopro from the wearer, otherwise we'll just keep seeing the last known owner move around on the map
+                        payload[CameraNetEntity] = component.Mobile ?
+                            (GetNetEntity(uid), GetNetCoordinates(_transform.ToCoordinates(uid, _transform.ToMapCoordinates(transformComponent.Coordinates)))) :
+                            (GetNetEntity(uid), GetNetCoordinates(transformComponent.Coordinates));
+                        payload[CameraMobile] = component.Mobile;
                     }
                     // Goobstation end
                     payload[DeviceNetworkConstants.Command] = CameraDataMessage;